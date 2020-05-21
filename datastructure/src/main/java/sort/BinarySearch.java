d5ca6d76fdc0f3ec0287560ea07eeb0258078d71
lvronggit <lvrongcom@163.com>
2020-05-21 19:20:20 +0800

    一些基础的排序

---
+++ datastructure/src/main/java/sort/BinarySearch.java
@@ -0,0 +1,14 @@
+package sort;
+
+import java.util.HashMap;
+
+/**
+ * 二分法
+ */
+public class BinarySearch {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ datastructure/src/main/java/sort/O1Lru.java
@@ -0,0 +1,18 @@
+package sort;
+
+import java.util.HashMap;
+
+/**
+ * 时间为O1的lru算法
+ */
+public class O1Lru {
+
+    public static void main(String[] args) {
+
+        new HashMap();
+
+
+    }
+
+
+}
