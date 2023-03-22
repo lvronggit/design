e06504102923aac30621537cd9e46fe45ff49dbf
rong.lv <rong.lv@huifu.com>
2023-03-22 16:57:34 +0800

    nio

--- nio/src/main/java/channel/FileChinnelDemo.java
+++ nio/src/main/java/channel/FileChinnelDemo.java
@@ -1,35 +1,61 @@
 package channel;
 
+import org.junit.Test;
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
 
 public class FileChinnelDemo {
-
-    public void copy() throws IOException {
-
-        FileChannel channel = new FileInputStream("C:\\Users\\lvrong\\Desktop\\日志.txt").getChannel();
-        ByteBuffer byteBuffer = ByteBuffer.allocate((int) channel.size());
-        int len = channel.read(byteBuffer);
-        System.out.println(len);
-        FileChannel channel2 = new FileOutputStream("C:\\Users\\lvrong\\Desktop\\llllllll.txt").getChannel();
-        byteBuffer.flip();
-        System.out.println((char) byteBuffer.get());
-        channel2.write(byteBuffer);
-        channel2.close();
+    /**
+     * 读取文件数据
+     * @throws IOException
+     */
+    @Test
+    public void read() throws IOException {
+        FileChannel channel = new FileInputStream("C:\\Users\\rong.lv\\Desktop\\niofile.txt").getChannel();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(8);
+        while (channel.read(byteBuffer)>0){
+            byteBuffer.flip();
+            String str = StandardCharsets.UTF_8.decode(byteBuffer).toString();
+            System.out.println(str);
+            byteBuffer.clear();
+        };
+        channel.close();
         byteBuffer.clear();
     }
 
+    /**
+     * 复制文件数据
+     * @throws IOException
+     */
+    @Test
+    public void copy() throws IOException {
+        FileChannel channel = new FileInputStream("C:\\Users\\rong.lv\\Desktop\\niofile.txt").getChannel();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        FileChannel channel2 = new FileOutputStream("C:\\Users\\rong.lv\\Desktop\\niofile - 副本.txt").getChannel();
+        int len = 0;
+        while ( (len = channel.read(byteBuffer))>0){
+            byteBuffer.flip();
+            channel2.write(byteBuffer);
+            byteBuffer.flip();
+        }
+        channel2.close();
+        channel.close();
+        byteBuffer.clear();
+    }
 
+    @Test
     public void copy2() throws IOException {
         FileChannel channel = new FileInputStream("C:\\Users\\lvrong\\Desktop\\日志.txt").getChannel();
         FileChannel channel2 = new FileOutputStream("C:\\Users\\lvrong\\Desktop\\2222222.txt").getChannel();
         channel2.transferFrom(channel, 0,channel.size());
 
     }
-
+    @Test
     public void copy3() throws IOException {
         FileChannel channel = new FileInputStream("C:\\Users\\lvrong\\Desktop\\日志.txt").getChannel();
         FileChannel channel2 = new FileOutputStream("C:\\Users\\lvrong\\Desktop\\222222222222.txt").getChannel();
@@ -38,9 +64,4 @@ public class FileChinnelDemo {
     }
 
 
-    public static void main(String[] args) throws IOException {
-        FileChinnelDemo fileChinnelDemo = new FileChinnelDemo();
-        fileChinnelDemo.copy2();
-        fileChinnelDemo.copy3();
-    }
 }
